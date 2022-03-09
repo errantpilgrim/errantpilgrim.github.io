edfabd1258be79beb243dffead765f57d97ffaf3
errantpilgrim <101091479+errantpilgrim@users.noreply.github.com>
2022-03-08 18:03:12 -0800

    -ga

--- v1/stvn.js
+++ v1/stvn.js
@@ -4,13 +4,6 @@ if (navigator.appVersion.indexOf('Windows') != -1)
 	if (window.chrome) document.write('<link rel="stylesheet" type="text/css" href="/directwrite.css">');
 }
 
-(function()
-{
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-})();
-
 window.addEventListener('load', function()
 {
     document.getElementById('skrollr-body').className = 'loaded';
